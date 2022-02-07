cda03076815e23f2ba16590a8bbfca56e912e251
Iulia Mieiță <mieitaiulia19@stud.ase.ro>
2022-02-07 13:26:40 +0200

    Added update member

--- front/ships/src/components/FormMember.js
+++ front/ships/src/components/FormMember.js
@@ -2,26 +2,40 @@ import React, { useEffect, useState } from "react"
 import axios from "axios"
 import { useParams, useNavigate } from "react-router-dom"
 
-function FormMember({ close , click, refresh}) {
-    const [name, setName] = useState("");
-    const [role, setRole] = useState("")
-    const {id} = useParams();
+function FormMember({ refresh, operation, _name, _role, _idMember }) {
+    const [name, setName] = useState(_name);
+    const [role, setRole] = useState(_role);
+    const { id } = useParams();
+
     const [message, setMessage] = useState(false);
-    
+
     const [visibility, setVisibility] = useState(false);
 
 
     const addMember = () => {
-        if (name !== "" && role!="") {
-            axios.post(`http://localhost:8080/app/ships/${id}/member`, { name: name, role: role })
-                .then(() => {
-                    setName("")
-                    setRole("");
-                    setMessage("")
-                    refresh();
-                })
-        } else {
-            setMessage("Incorect data")
+        console.log(name, role)
+        if (operation === "ADD") {
+            if (name !== "" && role != "") {
+                axios.post(`http://localhost:8080/app/ships/${id}/member`, { name: name, role: role })
+                    .then(() => {
+                        setName("")
+                        setRole("");
+                        setMessage("")
+                        refresh();
+                    })
+            } else {
+                setMessage("Incorect data")
+            }
+        }
+        if (operation === "UPDATE") {
+            if (name !== "" && role != "") {
+                axios.put(`http://localhost:8080/app/ships/${id}/members/${_idMember}`, { name: name, role: role })
+                    .then((response) => {
+                        console.log(response.data);
+                        setMessage("")
+                        refresh();
+                    })
+            }
         }
     }
 
@@ -32,18 +46,38 @@ function FormMember({ close , click, refresh}) {
     const handleRole = (event) => {
         setRole(event.target.value);
     }
-    return (
-        <>
-            <h3>Form add new member to ship:</h3>
-            <input type="text" placeholder="Name" onChange={handleName} value={name}></input>
-            <select name="role" onChange={handleRole}>
-                <option value="CAPTAIN">Captain</option>
-                <option value="BOATSWAIN">Boatswain</option>
-            </select>
-            {message}
-            <button type="submit" onClick={() => addMember()}>Create</button>
-        </>
-    ) 
+    if ( operation  === "ADD") {
+        return (
+            <>
+                <h3>Form {operation} member to ship:</h3>
+                <input type="text" placeholder="Name" onChange={handleName} ></input> <br />
+                <select name="role" onChange={handleRole}>
+
+                    <option value="CAPTAIN">Captain</option>
+                    <option value="BOATSWAIN">Boatswain</option>
+                </select>
+                {message}
+                <button type="submit" onClick={() => addMember()}>{operation}</button>
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                <h3>Form {operation} member to ship:</h3>
+                <h4>Old value: {_name}</h4> 
+                <input type="text" placeholder="Name" onChange={handleName} ></input> <br />
+                <h4>Old value: {_role}</h4>
+                <select name="role" onChange={handleRole}>
+
+                    <option value="CAPTAIN">Captain</option>
+                    <option value="BOATSWAIN">Boatswain</option>
+                </select>
+                {message}
+                <button type="submit" onClick={() => addMember()}>{operation}</button>
+            </>
+        )
+    }
 }
 
 export default FormMember;
--- front/ships/src/components/ListMembers.js
+++ front/ships/src/components/ListMembers.js
@@ -1,31 +1,35 @@
-import React, {useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios"
 import FormMember from "./FormMember"
 
 //pt heroku later !! nu esti pe 8080 pe front, ci pe server
 const SERVER = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`
-function ListMembers(){
+function ListMembers() {
     const [members, setMembers] = useState([]);
     const [ship, setShip] = useState([]);
     const [refresh, setRefresh] = useState(false);
     const [click, setClick] = useState(false)
-    let {id} = useParams();
+    const [operation, setOperation] = useState("")
+    const [name, setName] = useState("");
+    const [role, setRole] = useState("")
+    const [idMember, setIdMember] = useState("")
+    let { id } = useParams();
     const navigate = useNavigate();
 
     const getMembers = () => {
         axios.get(`http://localhost:8080/app/ships/${id}/member`)
-        .then((response) => {
-            setMembers(response.data.Members)
-            setShip(response.data.name, response.data.displacement)
-        })
-        .catch((response) => {
-            console.log(response);
-        })
+            .then((response) => {
+                setMembers(response.data.Members)
+                setShip(response.data.name, response.data.displacement)
+            })
+            .catch((response) => {
+                console.log(response);
+            })
     }
 
     useEffect(() => {
-       getMembers();
+        getMembers();
     }, [refresh])
 
     const redirect = (id) => {
@@ -39,26 +43,37 @@ function ListMembers(){
     const deleteMember = (id_member) => {
         console.log(id_member, id)
         axios.delete(`http://localhost:8080/app/ships/${id}/members/${id_member}`)
-        .then((response) => {
-            handleRefresh();
-        })
-        .catch((response) => {
-            console.log(response);
-        })
-     }
+            .then((response) => {
+                handleRefresh();
+            })
+            .catch((response) => {
+                console.log(response);
+            })
+    }
 
 
-     const updateMember = (id) => {
-         setClick(true)
-     }
+    const updateMember = (id, name, role) => {
+        setIdMember(id);
+        setName(name);
+        setRole(role);
+        setClick(true)
+        setOperation("UPDATE")
+    }
+
+    const addMember = (id) => {
+        setName("");
+        setRole("");
+        setClick(true)
+        setOperation("ADD")
+    }
 
 
-    return(
+    return (
         <>
-        <h1>Details ship</h1>
-        <h3>Ship name: {ship}</h3>
-        <h4>Echipaj:</h4>
-        <table>
+            <h1>Details ship</h1>
+            <h3>Ship name: {ship}</h3>
+            <h4>Echipaj:</h4>
+            <table>
                 <thead>
                     <tr>
                         <th>Id</th>
@@ -68,22 +83,23 @@ function ListMembers(){
                     </tr>
                 </thead>
                 <tbody>
-                {members.map((item, key) => (
+                    {members.map((item, key) => (
                         <tr key={key}>
                             <td>{item.id}</td>
                             <td>{item.name}</td>
                             <td>{item.role}</td>
                             <td>
-                            <button onClick={() => deleteMember(item.id)}>delete</button>
-                            <button onClick={() =>updateMember(item.id)}>update</button>
+                                <button onClick={() => deleteMember(item.id)}>delete</button>
+                                <button onClick={() => updateMember(item.id, item.name, item.role)}>update</button>
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
             <button id="back" onClick={() => redirect()}>back</button>
+            <button id="addMember" onClick={() => addMember()}>new member</button>
             <div className="form-member">
-                {click ? <FormMember  refresh={handleRefresh}/> : null}
+                {click ? <FormMember refresh={handleRefresh} operation={operation} _name={name} _role={role} _idMember={idMember} /> : null}
             </div>
         </>
     )
